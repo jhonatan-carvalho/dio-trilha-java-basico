4735e8cba6abe60c011e7d008173a7e448807add
Jhonatan Carvalho <jhonatan.carvalho@protonmail.com>
2025-08-14 14:37:43 -0300

    Desenvolvimento do código para o desafio.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        /*
+        Conhecer e importar a classe Scanner
+        Exibir as mensagens para o usuário
+        Obter os valores digitados pelo terminal
+        Exibir mensagem de conta criada
+        */
+
+        var scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        Double saldo = 3000.00;
+
+        System.out.println("Informe os dados a seguir:");
+        System.out.print("Nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.print("Agência: ");
+        agencia = scanner.next();
+
+        System.out.print("Número da conta: ");
+        numeroConta = scanner.nextInt();
+
+        String saida = "Olá" + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                .concat(agencia)
+                .concat(", conta ")
+                .concat(String.valueOf(numeroConta))
+                .concat(" e seu saldo ")
+                .concat(String.valueOf(saldo))
+                .concat(" já está disponível para saque");
+
+        System.out.printf(saida);
 
     }
 }
